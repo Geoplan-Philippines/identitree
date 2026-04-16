85373d9f59d96beb2353993d5492f8eb72886c1c
Chester Luke Maligaso <123941342+Kukaas@users.noreply.github.com>
2026-04-16 08:29:13 +0800

    Fix env import and use  auth url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from './configs/env';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+    origin: [env.authUrl, 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
